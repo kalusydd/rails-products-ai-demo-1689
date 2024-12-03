20fadf4ea938f9c6717fed37d0774c755fdf32aa
Kat Syddall <kat.syddall@gmail.com>
2024-12-03 17:33:46 +0100

    uodated puma

--- config/puma.rb
+++ config/puma.rb
@@ -43,6 +43,6 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
 
-if Rails.env.development?
+if rails_env == "development"
   plugin :solid_queue
 end
